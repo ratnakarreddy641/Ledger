8d4fdf95a00c5bf5ffeb5ba47a7e421db3465e08
ratnakarreddy641 <ratnu641@gmail.com>
2023-11-06 00:22:23 +0530

    Added Login

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { auth } from '../Google/config';
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='flex items-center justify-between m-5 '>
+      <div className='Title w-fit relative'>
+        <h1 className="text-center text-4xl font-black	"  >Ledger</h1>
+        <h1 className='absolute text-xs right-0 -bottom-5'>By Epic Poet</h1>
+      </div>
+      <div className='flex items-center bg-slate-200 rounded-3xl px-3 py-2 border hover:border-slate-400 cursor-pointer'>
+        <h1 className='text-xs'>{auth.currentUser.displayName}</h1>
+        <img className='w-8 rounded-full mx-1' src={auth.currentUser.photoURL} alt="User Image" />
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GoogleButton from 'react-google-button'
+import { auth,provider } from '../Google/config'
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+  const[user, setUser]=useState('');
+  const navigate = useNavigate();
+
   const login = ()=>{
-    
+    signInWithPopup(auth,provider).then((result)=>{
+      setUser(auth.currentUser.displayName);
+      navigate("/dashboard");
+             
+    })
   }
+  useEffect(()=>{},[user]);
 
 
   return (
@@ -15,9 +26,15 @@ function Login() {
             <h1 className="text-center text-5xl font-black	"  >Ledger</h1>
             <h1 className='absolute right-0 -bottom-7'>By Epic Poet</h1>
           </div>
-          <GoogleButton className="my-10" type='light'/>
+          {
+            !auth.currentUser&&<GoogleButton className="my-10" onClick={login} type='light'/>
+            ||<button className='rounded-3xl bg-slate-200 text-xs p-3 my-10 border hover:border-slate-400' 
+            onClick={()=>{navigate("/dashboard")}}
+            >Dashboard</button>
+          }
+          
         </div>
-
+        
       </div>
     </div>
   )
